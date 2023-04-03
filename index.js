47b91fbc467d5bb7b07c5d4d583c7a5a34aff220
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-04-02 21:01:04 -0400

    Acting On Events Lab

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ function moveDodgerLeft() {
 document.addEventListener("keydown", function (e) {
   if (e.key === "ArrowLeft") {
     moveDodgerLeft();
+    
   }
   if (e.key === "ArrowRight"){
     moveDodgerRight();
